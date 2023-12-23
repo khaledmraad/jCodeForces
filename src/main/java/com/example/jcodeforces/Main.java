36f1e5d3ccc8b0c085d2403eb11f37042b282126
khaledmraad <khaled.mrad@ensi-uma.tn>
2023-12-24 00:24:31 +0100

    label added

--- src/main/java/com/example/jcodeforces/Main.java
+++ src/main/java/com/example/jcodeforces/Main.java
@@ -1,6 +1,9 @@
 package com.example.jcodeforces;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.*;
@@ -11,6 +14,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Objects;
+import java.util.Vector;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -20,6 +24,7 @@ import org.json.simple.parser.JSONParser;
 
 public class Main extends Application {
 
+    Stage window;
     public static void main(String[] args) {
         launch(args);
     }
@@ -59,11 +64,16 @@ public class Main extends Application {
 
 //        System.out.print(contests);
 
+
+        Vector<Label> textLabel=new Vector<>();
+
         for (int i=0;i<contests.size();i++){
             JSONObject ithContest=(JSONObject) contests.get(i);
 
             String contestState=(String) ithContest.get("phase");
             if(Objects.equals(contestState, "BEFORE")){
+                Label l=new Label((String) ithContest.get("name"));
+                textLabel.add(l);
                 System.out.println((String) ithContest.get("name") );
             }
             else {
@@ -71,6 +81,15 @@ public class Main extends Application {
             }
 
         }
+
+        window=primaryStage;
+        VBox vBox=new VBox(20);
+        vBox.getChildren().addAll(textLabel);
+
+
+        Scene scene=new Scene(vBox,300,300);
+        window.setScene(scene);
+        window.show();
 
     }
 }
